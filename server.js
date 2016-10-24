a5a84ffd6004f4c85eb6b183157276d4c9d08043
CJT1105 <CJT1105@GMAIL.COM>
2016-10-24 12:19:04 -0500

    redeploy

--- server.js
+++ server.js
@@ -18,8 +18,7 @@ app.use(session({
     secret: 'keyboard cat',
     cookie: {secure: false},
     store: new RedisStore({
-    url:  'redis://redistogo:1136da6a4e2373b0922e8bc2dc4debe8@sculpin.redistogo.com:10579/
-',
+    url:  'redis://redistogo:1136da6a4e2373b0922e8bc2dc4debe8@sculpin.redistogo.com:10579/',
     resave: true,
     saveUninitialized: true
   })
