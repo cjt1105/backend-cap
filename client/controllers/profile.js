f99a7e3ae0c9ec39d9a92d600d73e5bb74abd937
CJT1105 <CJT1105@GMAIL.COM>
2016-11-03 18:37:43 -0500

    added styling and changed user and account models

--- client/controllers/profile.js
+++ client/controllers/profile.js
@@ -18,7 +18,7 @@ angular.module('streamBuddies')
             $scope.user = data;
             $scope.cardAdded = !data.card_added
             console.log($scope.cardAdded)
-            // $scope.$apply()
+            $scope.$apply()
         })
     })
 
