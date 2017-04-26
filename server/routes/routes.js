331d074c5ebefcefc13fbe01ef26f6e31a7c16dd
CJT1105 <CJT1105@GMAIL.COM>
2017-04-26 13:36:21 -0500

    cleaned up routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,20 +3,22 @@ const router = Router();
 const passport = require('passport')
 const config = require('../middlewares/config/passport')(passport);
 const auth = require('../middlewares/config/auth.js');
-const graph = require('fbgraph');
-const { getDate , shouldExchange } = require('../helpers/date')
-const request = require('request')
-const Invite = require('../models/invite');
-const stripe = require('stripe')('sk_test_ZaWMUUXlFjKGoG4VyftGyCQ9')
-const timestamp = require('unix-timestamp')
-const Account = require('../models/account');
-const { User } = require('../models/user')
+const stripe = require('stripe')('sk_test_ZaWMUUXlFjKGoG4VyftGyCQ9');
 const Users = require('../controllers/user.js')
-
-//imports
 const Accounts = require('../controllers/account.js')
 const  Invites  = require('../controllers/invite.js')
 const Stripe = require('../controllers/stripe.js')
+
+//Auth routes
+router.get('/login/facebook', passport.authenticate('facebook', { scope : ['user_friends', 'publish_actions'], display: 'popup' }))
+
+router.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login', successRedirect: '/' }))
+
+router.get('/logout', (req,res) => {
+    req.logout();
+    req.session.destroy();
+    res.redirect('/')
+})
 
 router.get('/loggedIn', (req,res) => {
     if(req.session.passport != undefined || null) {
@@ -26,11 +28,6 @@ router.get('/loggedIn', (req,res) => {
     }
 })
 
-//Auth routes
-router.get('/login/facebook', passport.authenticate('facebook', { scope : ['user_friends', 'publish_actions'], display: 'popup' }))
-
-router.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login', successRedirect: '/' }))
-
 // User routes
 router.get('/api/user/info', Users.fetchUserInfo)
 
@@ -38,24 +35,18 @@ router.get('/api/user/accounts', Users.fetchAccounts )
 
 router.get('/api/subscriptions', Users.fetchSubcriptions)
 
+router.get('/api/user/accounts/:id', Users.fetchSingleAccount)
+
 //Account routes
 router.get('/accounts/populate', Accounts.populate )
 
-router.post('/accounts/add', Accounts.add )
-
-
-
-router.get('/api/user/accounts/:id', Users.fetchSingleAccount)
-
-router.get('/logout', (req,res) => {
-    req.logout();
-    req.session.destroy();
-    res.redirect('/')
-})
-
-router.get('/api/accounts/invite/:id', Invites.fetchOne )
+router.post('/accounts/add', Accounts.create)
 
 router.patch('/api/accounts/addUser', Accounts.addUser )
+
+router.post('/api/accounts/subscribeUser', Accounts.subscribeUser )
+
+// Invite routes
 
 router.post('api/deleteInvite', Invites.delete )
 
@@ -63,9 +54,9 @@ router.post('/api/invites', Invites.add )
 
 router.get('/api/invites', Invites.fetchAll )
 
-router.post('/api/stripe/createUser', Users.createStripeUser )
+router.get('/api/accounts/invite/:id', Invites.fetchOne )
 
-router.post('/api/accounts/subscribeUser', Accounts.subscribeUser )
+router.post('/api/stripe/createUser', Users.createStripeUser )
 
 router.get('/delete/:stripeId', (req,res) => {
     const stripeId = req.params.stripeId
@@ -74,4 +65,4 @@ router.get('/delete/:stripeId', (req,res) => {
 
 router.post('/stripe/events', Stripe.eventListener)
 
-  module.exports = router;
+module.exports = router;
